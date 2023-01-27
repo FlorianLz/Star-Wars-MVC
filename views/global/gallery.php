66f3d53ef18eef328a5bcdf6d21d12c5370fff11
delacherie olivier <olivier28111999@hotmail.fr>
2023-01-27 14:17:56 +0100

    correction affichage

--- views/global/gallery.php
+++ views/global/gallery.php
@@ -10,7 +10,6 @@
 <section class="galery">
     <div class="container">
         <div class="galery--content">
-            <h1 class="title">Galerie photo</h1>
             <div class="galery--list">
                 <?php foreach ($galleries as $gallery) { ?>
                     <div class="galery--item">
