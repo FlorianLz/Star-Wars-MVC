716733ab78b5c798e9b3def023cda183a27c2f50
delacherie olivier <olivier28111999@hotmail.fr>
2023-01-27 11:42:42 +0100

    correction bug affichage chrome

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,4 +1,9 @@
         </div>
+        <footer class="footer">
+            <div class="footer--content">
+
+            </div>
+        </footer>
         <script src="/public/js/script.js" ></script>
     </body>
 </html>
